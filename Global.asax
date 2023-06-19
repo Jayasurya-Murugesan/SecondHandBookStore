318ff7b2f422dd5072ffb2372f6100e5ccb8d705
Jayasurya <inboxofsurya@gmail.com>
2023-06-19 13:35:02 +0530

    Initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SeondHandBookStorePortal.MvcApplication" Language="C#" %>
---
+++ Models/Registration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SeondHandBookStorePortal.Models
+{
+    public class Registration
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
